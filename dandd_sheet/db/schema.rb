4d86691f342824b45a089507a7d1d67dfa87cc55
Harley McDowell <hmcdowell95@gmail.com>
2020-05-07 05:27:52 +0000

    saving2

--- dandd_sheet/db/schema.rb
+++ dandd_sheet/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "characters", force: :cascade do |t|
     t.string "class_name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "info"
     t.integer "ac"
     t.integer "hit_points"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
